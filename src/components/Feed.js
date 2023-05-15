7f9e3438c6460d0ccae5b3e8c372f66483420f20
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-05-15 19:57:32 +0300

    Add Search bar

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 function Feed() {
   return (
-    <div>Feed</div>
-  )
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Typography className="copyright" variant = "body2" sx = {{ mt: 1.5, color: '#fff'}}>
+          Copyright 2023 zlatanov-dev
+        </Typography>
+      </Box>
+    </Stack>
+  );
 }
 
-export default Feed
+export default Feed;
