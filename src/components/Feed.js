6842d1d52263cb54aac2ae643f04042eb1298530
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-06-03 11:53:09 +0300

    Add Video Card and Channel Card

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,7 +1,22 @@
-import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import { SideBar, Videos } from "./";
+import { fetchFromApi } from "../utils/fetchFromApi";
+import { useEffect, useState } from "react";
+
+
 
 function Feed() {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  
+  }, [selectedCategory]);
+
+
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -11,9 +26,18 @@ function Feed() {
           px: { sx: 0, md: 2 },
         }}
       >
+        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
         <Typography className="copyright" variant = "body2" sx = {{ mt: 1.5, color: '#fff'}}>
           Copyright 2023 zlatanov-dev
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}}>
+          {selectedCategory} <span style={{color: '#F31503'}}> videos</span>
+        </Typography>
+        
+        <Videos videos={videos}/> 
       </Box>
     </Stack>
   );
